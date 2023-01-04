6491ecb02db7247a82b856d2976e090adf7c08aa
ahenches <arnaud.henches@gmail.com>
2023-01-04 06:06:02 +0100

    La fonction determinisation marche bientot in she allah

--- automate.h
+++ automate.h
@@ -14,31 +14,32 @@ typedef struct Etats_N_det {
 	int etats[MAX_NOMBRE_ETATS_NON_DETERMINE];
 } etats_N_det;
 
+bool est_set_etats_egaux(etats_N_det * etat1, etats_N_det * etat2);
+int indice_set_etats_dans_tableau(etats_N_det * etats, etats_N_det ** liste_set_etats, int taille_liste);
+
 typedef struct Alphabet {
 	int nombre_lettres;
 	char *lettres;
+	int correspondance[CARACTERES_IMPRIMABLES];
 } alphabet;
 
 typedef struct AFN {
 	etats_N_det ** etats_transitions;
-	int correspondance[CARACTERES_IMPRIMABLES];
 	bool * sont_etats_finals;
 	int nombre_etats;
 	alphabet alphabet;
 } afn;
 
 typedef struct AFD {
-    int nbEtats;
+    int nombre_etats;
     bool *etats;
     int **transitions;
 	alphabet alphabet;
 }afd;
 
 afn genererAFN(char *file_name);
-afd nouveauAFD(int nbEtats);
+afd nouveauAFD(int nombre_etats, alphabet * alphabet);
+afd determinisationAFN(afn * anf_a_determiniser);
 void afficherAFD(afd *afd);
 bool executer_AFN_rec(int etat_actuel, char chaine_restante[], afn *afn, int profondeur); // return 1 si mot restant valide PEUT ETRE RECURSIVE OU NON 
 bool executer_AFD_rec(int etat_actuel, char chaine_restante[], afd *afd, int profondeur); // return 1 si mot restant valide PEUT ETRE RECURSIVE OU NON 
-
-
-
--- main.c
+++ main.c
@@ -7,7 +7,27 @@
 int main( int argc, char *argv[ ] )
 {
 	afn afn_ = genererAFN(argv[1]);	
-
+	etats_N_det etats_0, etats_1, e1= {0}, e2 = {0}, e3= {0}, e4= {0};
+	e2.nombre_etats_N_det = 0;
+	e1.etats[0] = 5;
+	e1.nombre_etats_N_det++;
+	etats_N_det * l[5];
+	l[3] = &etats_1;
+	l[0] = &e1;
+	l[1] = &e2;
+	l[2] = &e4;
+	l[4] = &e3;
+	etats_1.etats[0] = 0;
+	etats_1.etats[1] = 5;
+	etats_1.etats[2] = 4;
+	etats_1.etats[3] = 1;
+	etats_0.etats[0] = 0;
+	etats_0.etats[1] = 5;
+	etats_0.etats[2] = 4;
+	etats_0.etats[3] = 1;
+	etats_1.nombre_etats_N_det = 4;
+	etats_0.nombre_etats_N_det = 4;
+	//printf("%d", indice_set_etats_dans_tableau(&etats_0, l, 5));
 	for(int i = 2; i < argc; i++)
 	{
 		// parcours de l'afn
@@ -17,11 +37,20 @@ int main( int argc, char *argv[ ] )
 		else
 			printf("Mot NON Valide !\n");
 	}
+	afd afd_ = determinisationAFN(&afn_);
+	afficherAFD(&afd_);
 	// determinisation
+	/*
 	for(int i = 2; i < argc; i++)
 	{
 		// parcours de l'afd
+		printf("%s\n", argv[i]);
+		if (executer_AFD_rec(0, argv[i], &afd_, 0))
+			printf("Mot Valide !\n");
+		else
+			printf("Mot NON Valide !\n");
 	}
+	*/
 	// minimalisation
 	for(int i = 2; i < argc; i++)
 	{
@@ -113,6 +142,8 @@ int main( int argc, char *argv[ ] )
 
 	//////////////////////////////////////////////////////////////////////////
 
+	// NE PAS OUBLIER DE FREE LA MEMOIRE
+
 	return 0;
 }
 
@@ -128,7 +159,7 @@ afn genererAFN(char *filename)
 	afn1.alphabet.lettres = (char*)malloc(CARACTERES_IMPRIMABLES*sizeof(char));
 	// initializing array elements
 	for (int i = 0; i < CARACTERES_IMPRIMABLES; i++)
-		afn1.correspondance[i] = -1;
+		afn1.alphabet.correspondance[i] = -1;
 	// LECTURE FICHIER
 	ptr = fopen(filename, "r");
 	if (NULL == ptr) {
@@ -165,12 +196,14 @@ afn genererAFN(char *filename)
 	{
 		res = strtok(line, delim);
 		res = strtok(NULL, delim);
-		if (afn1.correspondance[*res-' '] == -1)
+		if (afn1.alphabet.correspondance[*res-' '] == -1)
 		{
 			afn1.alphabet.lettres[afn1.alphabet.nombre_lettres] = *res;
-			afn1.correspondance[*res-' '] = afn1.alphabet.nombre_lettres++;
+			afn1.alphabet.correspondance[*res-' '] = afn1.alphabet.nombre_lettres++;
 		}
 	}
+	free(afn1.alphabet.lettres);
+	afn1.alphabet.lettres = (char*)malloc(afn1.alphabet.nombre_lettres*sizeof(char));
 	fseek(ptr, save, SEEK_SET);
 	
 	for(int i = 0; i < afn1.alphabet.nombre_lettres; i++)
@@ -191,7 +224,7 @@ afn genererAFN(char *filename)
 		etat_d = atoi(strtok(line, delim));
 		res = strtok(NULL, delim);
 		etat_f = atoi(strtok(NULL, delim));
-		indice_lettre = afn1.correspondance[*res - ' '];
+		indice_lettre = afn1.alphabet.correspondance[*res - ' '];
 		indice_etat_f = afn1.etats_transitions[etat_d][indice_lettre].nombre_etats_N_det++;
 		afn1.etats_transitions[etat_d][indice_lettre].etats[indice_etat_f] = etat_f;
 	}
@@ -225,50 +258,138 @@ bool executer_AFN_rec(int etat_actuel, char chaine_restante[], afn *afn, int tai
 		bool mot_restant_valide = 0;
 		taille_retrait += compter_digit(etat_actuel) + strlen(chaine_restante) + 7;
 		int i = 0;
-		while(i < afn->etats_transitions[etat_actuel][afn->correspondance[*chaine_restante-' ']].nombre_etats_N_det && !mot_restant_valide)
+		while(i < afn->etats_transitions[etat_actuel][afn->alphabet.correspondance[*chaine_restante-' ']].nombre_etats_N_det && !mot_restant_valide)
 		{
 			if (i > 0)
 				afficher_n_fois(" ", taille_retrait);
 			printf("|- ");
-			mot_restant_valide = mot_restant_valide |  executer_AFN_rec(afn->etats_transitions[etat_actuel][afn->correspondance[*chaine_restante-' ']].etats[i], &chaine_restante[1], afn, taille_retrait);
+			mot_restant_valide = mot_restant_valide |  executer_AFN_rec(afn->etats_transitions[etat_actuel][afn->alphabet.correspondance[*chaine_restante-' ']].etats[i], &chaine_restante[1], afn, taille_retrait);
 			i++;
 		}
-		if (afn->etats_transitions[etat_actuel][afn->correspondance[*chaine_restante-' ']].nombre_etats_N_det == 0)
+		if (afn->etats_transitions[etat_actuel][afn->alphabet.correspondance[*chaine_restante-' ']].nombre_etats_N_det == 0)
 			 printf("|- ko \n");
 		return mot_restant_valide;
 	}
 }
 
-afd nouveauAFD(int nbEtats)
+afd nouveauAFD(int nombre_etats, alphabet * alphabet)
 {
     afd afd;
-
-    afd.nbEtats = nbEtats;
-    afd.etats = (bool *)malloc(nbEtats*sizeof(bool));
-    afd.transitions =  (int**)malloc(nbEtats*sizeof(int*));
-    for(int i = 0; i < nbEtats; i++)
+	afd.alphabet = *alphabet;
+    afd.nombre_etats = nombre_etats;
+    afd.etats = (bool *)malloc(nombre_etats*sizeof(bool));
+    afd.transitions =  (int**)malloc(nombre_etats*sizeof(int*));
+    for(int i = 0; i < nombre_etats; i++)
     {
-        afd.transitions[i] = (int*)malloc(CARACTERES_IMPRIMABLES*sizeof(int));
-    }
-    for(int i=0; i< nbEtats; i++)
-    {
-        afd.etats[i] = 0;
-        for(int j =0; j<CARACTERES_IMPRIMABLES; j++)
-        {
-            afd.transitions[i][j] = -1;
-        }
+		afd.etats[i] = 0;
+        afd.transitions[i] = (int*)malloc(alphabet->nombre_lettres*sizeof(int));
+		for (int j = 0; j<alphabet->nombre_lettres; j++)
+		{
+			afd.transitions[i][j] = -1;
+		}
     }
     return afd;
 }
 
 
+bool est_set_etats_egaux(etats_N_det * etat1, etats_N_det * etat2)
+{
+	
+		printf("\netats1\n");
+	for(int i = 0; i < etat1->nombre_etats_N_det; i++)
+	{
+		printf("%d ", etat1->etats[i]);
+	}
+		printf("\netats2\n");
+	for(int i = 0; i < etat2->nombre_etats_N_det; i++)
+	{
+		printf("%d ", etat2->etats[i]);
+	}
+	
+	if (etat1->nombre_etats_N_det != etat2->nombre_etats_N_det) return 0;
+	else 
+	{
+		for(int i = 0; i < etat1->nombre_etats_N_det; i++)
+		{
+			if (etat1->etats[i] != etat2->etats[i]) return 0;
+		}
+		return 1;
+	}	
+}
+
+int indice_set_etats_dans_tableau(etats_N_det * etats, etats_N_det ** liste_set_etats, int taille_liste)
+{
+	for(int i = 0; i < taille_liste; i++)
+	{
+		printf("INDICE %d\n", i);
+		if (est_set_etats_egaux(etats, liste_set_etats[i])) return i;
+	}
+	return -1;
+}
+
+afd determinisationAFN(afn * afn_a_determiniser)
+{
+	int nombre_max_etats = afn_a_determiniser->nombre_etats * afn_a_determiniser->alphabet.nombre_lettres+1;
+	afd afd_a_renvoyer = nouveauAFD(nombre_max_etats, &afn_a_determiniser->alphabet);
+	
+	etats_N_det * file[nombre_max_etats];
+
+	etats_N_det etats_0;
+	etats_0.etats[0] = 0;
+	etats_0.nombre_etats_N_det = 1;
+
+	file[0] = &etats_0;
+
+	int etat_actuel;
+	afd_a_renvoyer.nombre_etats = 1;
+	bool est_final;
+	int taille_file = 1, indice_parcours = 0, indice_dans_file;
+	
+	printf("iadzkhzdhdosj%d\n", afn_a_determiniser->alphabet.nombre_lettres);
+	while (indice_parcours < taille_file)
+	{
+		printf("indice_parcours%d\n", indice_parcours);
+		est_final = 0;
+		for (int j = 0; j < file[indice_parcours]->nombre_etats_N_det; j++)
+		{
+			etat_actuel = file[indice_parcours]->etats[j];
+			printf("etat_actuel%d\n", etat_actuel);
+			for(int indice_lettre = 0; indice_lettre < afn_a_determiniser->alphabet.nombre_lettres; indice_lettre++)
+			{
+				printf("indice_lettre%d\n", indice_lettre);
+				etats_N_det etats_transition = afn_a_determiniser->etats_transitions[etat_actuel][indice_lettre];
+				// Verification si etat_N_actuel est déjà dans file sinon on le rajoute
+				indice_dans_file = indice_set_etats_dans_tableau(&etats_transition, file, taille_file);
+				printf("indice_dans_file%d\n", indice_dans_file);
+				if (indice_dans_file == -1)
+				{
+					file[taille_file] = &etats_transition;
+					taille_file++;
+					
+					afd_a_renvoyer.transitions[indice_parcours][indice_lettre] = afd_a_renvoyer.nombre_etats;
+					afd_a_renvoyer.nombre_etats++;
+				}
+				else 
+				{
+					afd_a_renvoyer.transitions[indice_parcours][indice_lettre] = indice_dans_file;
+				}
+			}
+			if (afn_a_determiniser->sont_etats_finals[etat_actuel]) est_final = 1;
+		}
+		afd_a_renvoyer.etats[afd_a_renvoyer.nombre_etats] = est_final;
+		indice_parcours++;	
+	}
+	printf("taille file = %d\n", taille_file);
+	return afd_a_renvoyer;
+}
+
 void afficherAFD(afd *afd)
 {
     char caractereRecontre;
     int i;
-    printf("%d\n", afd->nbEtats);
+    printf("afd.nombre_etats %d\n", afd->nombre_etats);
     
-    for(i = 0; i < afd->nbEtats; i++)
+    for(i = 0; i < afd->nombre_etats; i++)
     {
         if(afd->etats[i] != 0)
         {
@@ -285,7 +406,7 @@ void afficherAFD(afd *afd)
     }while(caractereRecontre !='\0');
     printf("\n");
     
-    for(i = 0; i < afd->nbEtats; i++)
+    for(i = 0; i < afd->nombre_etats; i++)
     {
         printf("%d\t",i);
         for(int j = 0; j < afd->alphabet.nombre_lettres; j++){

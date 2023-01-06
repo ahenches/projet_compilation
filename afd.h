c01d923556fa4610b224d54de58c603d5ff7c3d1
maudlst <74672202+maudlst@users.noreply.github.com>
2023-01-06 16:56:53 +0100

    Ajout comment strucuture AFD

--- afd.h
+++ afd.h
@@ -5,6 +5,14 @@
 #define max(a,b) (a>=b?a:b)
 #define min(a,b) (a<=b?a:b)
 
+/**
+ * @brief Structure de donnée représentant un automate fini deterministe
+ * nombre_etats represente le nombre total d etas de l afd 
+ * etat est un tableau indexé parr les etats de l automate,de taille nombre_etat,  contenant des booleen, 1 si l état est terminal sinon 0
+ * transitions est un tableau 2D indexé par les etas de l automate et les caracteres de l'alphabet de l automate, il contient les etats d arrivée des transitions
+ * ex: la transition (0,a,1) est stocké : transitions[0]['a'] = 1, pour l'indice 'a' on utilise le tableau de corespondance de la structure alphabet 
+ * alphabet est une structure comportant les caracteres de l'alphabet de l automate
+ */
 typedef struct AFD {
     int nombre_etats;
     bool *etats;
